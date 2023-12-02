d999af725cba0002ab82044f70f5007b3aa1d836
Hamza <82061179+hxxmz@users.noreply.github.com>
2023-12-02 12:21:46 +0500

    20231202

--- CaseFileArrestFormatter/CaseFileArrestFormatter/CaseFileArrestFormatter.Designer.cs
+++ CaseFileArrestFormatter/CaseFileArrestFormatter/CaseFileArrestFormatter.Designer.cs
@@ -29,20 +29,340 @@ namespace CaseFileArrestFormatter
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Agent:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(91, 430);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(123, 45);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(189, 23);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(123, 74);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(189, 23);
+            this.textBox2.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Assists:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(40, 106);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 15);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Suspect:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(123, 103);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(189, 23);
+            this.textBox3.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(51, 138);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 15);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Date:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(171, 430);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Reset";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CustomFormat = "   dd-MM-yyyy";
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(123, 132);
+            this.dateTimePicker1.MaxDate = new System.DateTime(2030, 12, 31, 0, 0, 0, 0);
+            this.dateTimePicker1.MinDate = new System.DateTime(2010, 1, 1, 0, 0, 0, 0);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(189, 23);
+            this.dateTimePicker1.TabIndex = 11;
+            this.dateTimePicker1.Value = new System.DateTime(2023, 12, 2, 0, 0, 0, 0);
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(51, 164);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(34, 15);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Type:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Normal Arrest",
+            "High Risk Arrest"});
+            this.comboBox1.Location = new System.Drawing.Point(123, 161);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(189, 23);
+            this.comboBox1.TabIndex = 13;
+            this.comboBox1.Text = "Select...";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(32, 222);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(59, 15);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Weapons:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(123, 219);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(189, 23);
+            this.textBox4.TabIndex = 16;
+            this.textBox4.Text = "None";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(123, 190);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(189, 23);
+            this.textBox5.TabIndex = 15;
+            this.textBox5.Text = "None";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(44, 193);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(41, 15);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Drugs:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(50, 296);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(34, 15);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "Frisk:";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(123, 293);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(189, 23);
+            this.textBox6.TabIndex = 18;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(18, 325);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(77, 15);
+            this.label9.TabIndex = 21;
+            this.label9.Text = "Confiscation:";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(123, 322);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(189, 23);
+            this.textBox7.TabIndex = 20;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(18, 354);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(77, 15);
+            this.label10.TabIndex = 23;
+            this.label10.Text = "Last Charges:";
+            this.label10.Click += new System.EventHandler(this.label10_Click);
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(123, 351);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(189, 23);
+            this.textBox8.TabIndex = 22;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(12, 383);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(94, 15);
+            this.label11.TabIndex = 25;
+            this.label11.Text = "Arrest / Inmates:";
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(123, 380);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(189, 23);
+            this.textBox9.TabIndex = 24;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label12.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(110)))), ((int)(((byte)(110)))), ((int)(((byte)(255)))));
+            this.label12.Location = new System.Drawing.Point(82, 257);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(159, 19);
+            this.label12.TabIndex = 26;
+            this.label12.Text = "ARREST EVIDENCE";
+            this.label12.Click += new System.EventHandler(this.label12_Click);
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label13.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(110)))), ((int)(((byte)(110)))), ((int)(((byte)(255)))));
+            this.label13.Location = new System.Drawing.Point(106, 9);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(120, 19);
+            this.label13.TabIndex = 27;
+            this.label13.Text = "C A P T U R E D";
             // 
             // CaseFileArrestFormatter
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(336, 465);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
             this.Name = "CaseFileArrestFormatter";
-            this.Text = "FBI CID | Case File Arrest Formatter";
+            this.Text = "FBI CID | CF Arrest Formatter";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
     }
 }
 
--- CaseFileArrestFormatter/CaseFileArrestFormatter/CaseFileArrestFormatter.cs
+++ CaseFileArrestFormatter/CaseFileArrestFormatter/CaseFileArrestFormatter.cs
@@ -17,5 +17,88 @@ namespace CaseFileArrestFormatter
             InitializeComponent();
         }
 
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label12_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string openingTags = "[B][FONT=Trebuchet MS][CENTER][table=\"width: 550, class: grid, align: center\"]\n[TR=\"bgcolor: #1E1E1E\"]\n[TD][table=\"width: 550, align: center\"][tr]\n[td][IMG]'https://i.imgur.com/qO2AJf8.png'[/IMG][/td][/tr][tr]\n[td][Font=Book Antique][Size=7][color=#8d8dFF][B]C A P T U R E D[/B][/color][/Size][/Font][/td][/tr][/table]\n[table=\"width: 550, align: center\"][tr][td]\n[size=2][table=\"width: 500, class: grid, align: center\"][Left]";
+            
+            string agent = "[TR][TD][COLOR=\"#8d8dff\"][B]Agent Name:[/B][/COLOR][/TD][TD] [color=#DCDCDC]" + textBox1.Text + "[/COLOR][/TD][/TR]"; 
+            string assist = "[TR][TD][COLOR=\"#8d8dff\"][B]Assistants:[/B][/COLOR][/TD][TD] [color=#DCDCDC]" + textBox2.Text + "[/COLOR][/TD][/TR]"; 
+            string suspect = "[TR][TD][COLOR=\"#8d8dff\"][B]Suspect Full Name:[/B][/COLOR][/TD][TD] [color=#DCDCDC]" + textBox3.Text + "[/COLOR][/TD][/TR]";
+            string date = "[TR][TD][COLOR=\"#8d8dff\"][B]Date of Arrest:[/B][/COLOR][/TD][TD] [color=#DCDCDC]" + dateTimePicker1.Value.ToString().Substring(0, 10) + "[/COLOR][/TD][/TR]";
+            object type = "[TR][TD][COLOR=\"#8d8dff\"][B]Type of Arrest[/B][/COLOR][/TD][TD] [color=#DCDCDC]" + comboBox1.SelectedItem + "[/COLOR][/TD][/TR]";
+            string drugs = "[TR][TD][COLOR=\"#8d8dff\"][B]Drugs Confiscated:[/B][/COLOR][/TD][TD] [color=#DCDCDC]" + textBox5.Text + "[/COLOR][/TD][/TR]";
+            string weapons = "[TR][TD][COLOR=\"#8d8dff\"][B]Arms Confiscated:[/B][/COLOR][/TD][TD] [color=#DCDCDC]" + textBox4.Text + "[/COLOR][/TD][/TR]";
+
+            string mid = "[/Left][/table][/size][table=\"width: 500, class: grid, align: center\"][/table]\n[Font=Book Antique][Size=3][color=#8d8dFF][B][U]ARREST EVIDENCE[/U][/B][/color][/Size][/Font]\n";
+
+            string frisk = "[spoiler]\n[B][COLOR=\"#DCDCDC\"][FONT=Book Antiqua]FRISK[/FONT][/COLOR][/B]\n[spoiler][img]" + textBox6.Text + "[/img][/spoiler]";
+            string confiscation = "[B][COLOR=\"#DCDCDC\"][FONT=Book Antiqua]FRISK[/FONT][/COLOR][/B]\n[spoiler][img]" + textBox7.Text + "[/img][/spoiler]";
+            string charges = "[B][COLOR=\"#DCDCDC\"][FONT=Book Antiqua]FRISK[/FONT][/COLOR][/B]\n[spoiler][img]" + textBox8.Text + "[/img][/spoiler]";
+            string arrestInmates = "[B][COLOR=\"#DCDCDC\"][FONT=Book Antiqua]ARREST & INMATES[/FONT][/COLOR][/B]\n[spoiler][img]" + textBox9.Text + "[/img][/spoiler]";
+
+            //MessageBox.Show(agent + "\n" + assist +"\n" + suspect +"\n" + date +"\n" + type + "\n" + drugs + "\n" + weapons, "Output");
+            MessageBox.Show(openingTags, "Output");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "None";
+            textBox5.Text = "None";
+            textBox6.Text = "";
+            textBox7.Text = "";
+            textBox8.Text = "";
+            textBox9.Text = "";
+            dateTimePicker1.ResetText();
+            comboBox1.SelectedIndex = -1;
+        }
+
     }
 }
