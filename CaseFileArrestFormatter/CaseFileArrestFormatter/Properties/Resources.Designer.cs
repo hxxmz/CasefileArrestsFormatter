dfb2895546740be2ddda879c706052b022741a78
Hamza <82061179+hxxmz@users.noreply.github.com>
2023-12-02 15:02:23 +0500

    more adjustments

--- CaseFileArrestFormatter/CaseFileArrestFormatter/Properties/Resources.Designer.cs
+++ CaseFileArrestFormatter/CaseFileArrestFormatter/Properties/Resources.Designer.cs
@@ -79,15 +79,5 @@ namespace CaseFileArrestFormatter.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap output_onlinepngtools__1_ {
-            get {
-                object obj = ResourceManager.GetObject("output-onlinepngtools (1)", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
